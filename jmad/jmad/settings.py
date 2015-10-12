73b1be6e256ba3be0c70f4b315043a0f257cbd34
Pavel Karateev <lancelote.du.lac@gmail.com>
2015-10-12 12:05:27 +0300

    initial solos app, route to index view

--- jmad/jmad/settings.py
+++ jmad/jmad/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'solos'
 )
 
 ROOT_URLCONF = 'jmad.urls'
---
+++ jmad/solos/tests/test_urls.py
@@ -0,0 +1,14 @@
+from django.core.urlresolvers import resolve
+from django.test import TestCase
+
+from solos.views import index
+
+
+class SolosURLsTest(TestCase):
+
+    def test_root_url_uses_index_view(self):
+        """
+        Test that the root url of the site resolves to the correct view function
+        """
+        root = resolve('/')
+        self.assertEqual(root.func, index)
