5db71673e4c17b19474c8cde4853aee1f082b4bf
Pavel Karateev <lancelote.du.lac@gmail.com>
2015-10-12 16:37:38 +0300

    Adds filtering to index and model data to template

--- jmad/solos/views.py
+++ jmad/solos/views.py
@@ -4,5 +4,17 @@ from .models import Solo
 
 
 def index(request):
-    context = {'solos': Solo.objects.filter(instrument=request.GET.get('instrument', None))}
+    context = {'solos': []}
+
+    if request.GET.keys():
+        solos_queryset = Solo.objects.all()
+
+        if request.GET.get('instrument', None):
+            solos_queryset = solos_queryset.filter(instrument=request.GET.get('instrument', None))
+
+        if request.GET.get('artist', None):
+            solos_queryset = solos_queryset.filter(artist=request.GET.get('artist', None))
+
+        context['solos'] = solos_queryset
+
     return render_to_response('solos/index.html', context)
