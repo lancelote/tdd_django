0b3400323c7bee69873773b8c1658c33e39010c9
Pavel Karateev <lancelote.du.lac@gmail.com>
2015-10-19 17:07:20 +0300

    Integrates MusicBrainzNGS API

--- jmad/solos/models.py
+++ jmad/solos/models.py
@@ -1,17 +1,24 @@
 from django.core.urlresolvers import reverse
 from django.db import models
+from django.utils.text import slugify
 
-from albums.models import Track
+import musicbrainzngs as mb
+
+from albums.models import Album, Track
+
+mb.set_useragent('JMAD - http://jmad.us/', version='0.0.1')
 
 
 class Solo(models.Model):
-
     track = models.ForeignKey(Track)
     artist = models.CharField(max_length=100)
     instrument = models.CharField(max_length=50)
     start_time = models.CharField(max_length=20, blank=True, null=True)
     end_time = models.CharField(max_length=20, blank=True, null=True)
     slug = models.SlugField()
+
+    class Meta:
+        ordering = ['track', 'start_time']
 
     def get_absolute_url(self):
         return reverse('solo_detail_view', kwargs={
@@ -26,5 +33,48 @@ class Solo(models.Model):
             duration_string = '{}-{}'.format(self.start_time, self.end_time)
         return duration_string
 
-    class Meta:
-        ordering = ['track', 'start_time']
+    @classmethod
+    def get_artist_tracks_from_musicbrainz(cls, artist):
+        """
+        Create Album, Track, and Solo records for artists we find in the MusicBrainzNGS API
+
+        :param artist: an artist's name as a string to search for
+        :return: Queryset of Solos
+        """
+        search_results = mb.search_artists(artist)
+        best_result = search_results['artist-list'][0]
+        instrument = Solo.get_instrument_from_musicbrainz_tags(best_result['tag-list'])
+
+        for album_dict in mb.browse_releases(best_result['id'], includes=['recordings'])['release-list']:
+            album = Album.objects.create(
+                name=album_dict['title'],
+                artist=artist,
+                slug=slugify(album_dict['title'])
+            )
+
+            for track_dict in album_dict['medium-list'][0]['track-list']:
+                track = Track.objects.create(
+                    album=album, name=track_dict['recording']['title'],
+                    track_number=track_dict['position'],
+                    slug=slugify(track_dict['recording']['title'])
+                )
+
+                Solo.objects.create(
+                    track=track,
+                    artist=artist,
+                    instrument=instrument,
+                    slug=slugify(artist)
+                )
+
+        return Solo.objects.filter(artist=artist)
+
+    @classmethod
+    def get_instrument_from_musicbrainz_tags(cls, tag_list):
+        """
+        Return a single instrument from a list of dict-tags as returned in the MusicBrainzNGS API
+
+        :param tag_list: a list of dicts with keys 'count' and 'name'
+        :return: a string
+        """
+        instrument_map = {'pianist': 'piano', 'bassist': 'bass'}
+        return instrument_map[set(instrument_map.keys()).intersection([tag['name'] for tag in tag_list]).pop()]
--- jmad/solos/views.py
+++ jmad/solos/views.py
@@ -1,6 +1,10 @@
+import musicbrainzngs as mb
+
 from django.shortcuts import render_to_response
 
 from .models import Solo
+
+mb.set_useragent('JMAD - http://jmad.us/', version='0.0.1')
 
 
 def index(request):
@@ -9,13 +13,17 @@ def index(request):
     if request.GET.keys():
         solos_queryset = Solo.objects.all()
 
-        if request.GET.get('instrument', None):
-            solos_queryset = solos_queryset.filter(instrument=request.GET.get('instrument', None))
+        if request.GET.get('instrument'):
+            solos_queryset = solos_queryset.filter(instrument=request.GET['instrument'])
 
-        if request.GET.get('artist', None):
-            solos_queryset = solos_queryset.filter(artist=request.GET.get('artist', None))
+        artist_kwarg = request.GET.get('artist', None)
+        if artist_kwarg:
+            solos_queryset = solos_queryset.filter(artist=artist_kwarg)
 
         context['solos'] = solos_queryset
+
+        if context['solos'].count() == 0 and artist_kwarg:
+            context['solos'] = Solo.get_artist_tracks_from_musicbrainz(artist_kwarg)
 
     return render_to_response('solos/index.html', context)
 
