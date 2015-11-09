73e2f9d32129bc84f7f0466f5bf091ec4e5bda88
Pavel Karateev <lancelote.du.lac@gmail.com>
2015-11-09 13:04:25 +0300

    Adds viewset and serializer for Solos

---
+++ jmad/solos/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from .models import Solo
+
+
+class SoloSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Solo
--- jmad/solos/views.py
+++ jmad/solos/views.py
@@ -1,8 +1,10 @@
-import musicbrainzngs as mb
-
 from django.shortcuts import render_to_response
 
+import musicbrainzngs as mb
+from rest_framework import viewsets, mixins
+
 from .models import Solo
+from .serializers import SoloSerializer
 
 mb.set_useragent('JMAD - http://jmad.us/', version='0.0.1')
 
@@ -33,3 +35,9 @@ def solo_detail(request, album, track, artist):
         'solo': Solo.objects.get(slug=artist, track__slug=track, track__album__slug=album)
     }
     return render_to_response('solos/solo_detail.html', context)
+
+
+class SoloViewSet(viewsets.GenericViewSet, mixins.CreateModelMixin):
+
+    queryset = Solo.objects.all()
+    serializer_class = SoloSerializer
